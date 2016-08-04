6bba868075d421c28343f5fab165305536a0e12e
Arpith Siromoney <arpith@feedreader.co>
2016-08-04 12:22:39 -0400

    get api key from user

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,9 +1,32 @@
 var React = require('react');
 
 var HomePage = React.createClass({
+  getInitialState: function() {
+    return {apiKey: ''};
+  },
+
+  login: function(e) {
+    e.preventDefault();
+    console.log(this.state.apiKey);
+  },
+
+  onChange: function(e) {
+    this.setState({apiKey: e.target.value});
+  },
+
   render: function() {
     return <div>
-      <p>Welcome to Tulip!</p>
+    <p>Welcome to Tulip!</p>
+    <form onSubmit={this.login}>
+    <label htmlFor="API-key">API Key</label>
+    <input id="API-key" 
+      name="API-key" 
+      placeholder="Paste your API key here!" 
+      onChange={this.onChange} 
+      value={this.state.apiKey}>
+    </input>
+    <button>Sign In!</button>
+    </form>
     </div>;
   }
 });
