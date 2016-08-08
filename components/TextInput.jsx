fc1fd392bf26c05130fa52ff260fd1b34ca74e90
Arpith Siromoney <arpith@feedreader.co>
2016-08-08 11:13:11 -0400

    textinput style

--- components/TextInput.jsx
+++ components/TextInput.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 
 class TextInput extends React.Component {
   render() {
+    const style = {
+      WebkitAppearance: 'none',
+      fontFamily: 'inherit',
+      background: 'white',
+      color: '#4A525A',
+      borderColor: '#D1D0CE'
+    };
     return <input id={this.props.id}
       name={this.props.name}
       placeholder={this.props.placeholder}
       onChange={this.props.onChange}
+      style={style}
+      type="text"
     />;
   }
 }
