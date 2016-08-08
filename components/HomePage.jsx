c7a2301b55c5238685eb843086120c3a4993fa25
Arpith Siromoney <arpith@feedreader.co>
2016-08-08 14:53:20 -0400

    minor fix

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import SignInPage from './SignInForm.jsx';
+import SignInPage from './SignInPage.jsx';
 
 class HomePage extends React.Component {
   render() {
-    if !(localStorage.email && localStorage.apiKey) return <SignInPage />;
-    else return 
-    return <div>
+    if (!(localStorage.email && localStorage.apiKey)) return <SignInPage />;
+    else return <div>
       <p>Welcome back to Tulip!</p>
       <p>Something should happen here :)</p>
     </div>;
