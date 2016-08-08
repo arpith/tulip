98d5f3cf7aedf67e8293d507732db5dffbcba68e
Arpith Siromoney <arpith@feedreader.co>
2016-08-08 15:13:16 -0400

    figure out localstorage access later (breaks server rendering)

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -3,11 +3,7 @@ import SignInPage from './SignInPage.jsx';
 
 class HomePage extends React.Component {
   render() {
-    if (!(localStorage.email && localStorage.apiKey)) return <SignInPage />;
-    else return <div>
-      <p>Welcome back to Tulip!</p>
-      <p>Something should happen here :)</p>
-    </div>;
+    return <SignInPage />;
   }
 }
 
