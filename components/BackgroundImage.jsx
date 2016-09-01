b2cc3269e337142119ff23acb2b77a5846424f9d
Arpith Siromoney <arpith@feedreader.co>
2016-09-01 11:39:04 -0400

    full screen background images

--- components/BackgroundImage.jsx
+++ components/BackgroundImage.jsx
@@ -11,15 +11,36 @@ class BackgroundImage extends React.Component {
     
     const src = urls[Math.floor(Math.random() * urls.length)];
 
-    const style = {
+    const containerStyle = {
+      zIndex: '-1',
+      position: 'fixed',
+      top: '-50%',
+      left: '-50%',
+      width: '200%',
+      height: '200%'
+    };
+
+    const imgStyle = {
       position: 'fixed',
       top: 0,
       zIndex: -1000,
       backgroundColor: '#FFFEF4',
       width: '100%',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      margin: 'auto',
+      minWidth: '50%',
+      minHeight: '50%',
     };
 
-    return <img src={src} style={style} />;
+    return (
+      <div style={containerStyle}>
+        <img src={src} style={imgStyle} />
+      </div>
+    );
   }
 }
 
