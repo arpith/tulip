f868e93122f7ee446e378a97fdc0e73f9bb71c09
Arpith Siromoney <arpith@feedreader.co>
2016-09-01 11:43:42 -0400

    use react router's history

--- client.js
+++ client.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import createBrowserHistory from 'history/lib/createBrowserHistory';
-import { Router } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
 let mountNode = document.getElementById("react-mount");
 
-render(<Router history={createBrowserHistory()} routes={routes} />, mountNode);
+render(<Router history={browserHistory} routes={routes} />, mountNode);
