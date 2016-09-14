2671f850070b2bb9fe65ffb2d4206c492b3f70e5
Arpith Siromoney <arpith@feedreader.co>
2016-09-14 16:03:43 -0400

    use prod react buil

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,13 @@ module.exports = {
     path: path.join(__dirname, 'public'),
     filename: 'main.js',
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
   module: {
     loaders: [{ 
       test: /\.json$/,
