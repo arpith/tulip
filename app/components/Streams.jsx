a8c59d0759a757efb88b62445cd1b99072eaf1f5
Arpith Siromoney <arpith@feedreader.co>
2017-09-15 13:14:23 -0400

    Streams: add left padding

--- app/components/Streams.jsx
+++ app/components/Streams.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 function Streams({ streams }) {
+  const style = { paddingLeft: '2em' };
   const stream = (s) => <div key={s.stream_id}>{s.name}</div>;
-  return <div>{streams.map(stream)}</div>;
+  return <div style={style}>{streams.map(stream)}</div>;
 }
 
 export default connect((state) => {
