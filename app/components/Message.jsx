49d43336e303391e03564421360c636e7fc29d5d
Arpith Siromoney <arpith@feedreader.co>
2017-09-15 14:26:58 -0400

    Message: use moment.js for timestamp

--- app/components/Message.jsx
+++ app/components/Message.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import moment from 'moment';
 
 class Message extends React.Component {
   render() {
     return (
       <div>
         <h3>{this.props.message.sender_full_name}</h3>
-        <div>{moment(this.props.message.timestamp).fromNow()}</div>
+        <div>{moment(this.props.message.timestamp, 'X').fromNow()}</div>
         <img src={this.props.message.avatar_url}></img>
         <p>{this.props.message.content}</p>
       </div>
