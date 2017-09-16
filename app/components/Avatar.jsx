3cc393d246ff68a6f2bdc1388db86a4031b55c40
Arpith Siromoney <arpith@feedreader.co>
2017-09-15 21:21:24 -0400

    Avatar: smaller padding on right

--- app/components/Avatar.jsx
+++ app/components/Avatar.jsx
@@ -4,7 +4,7 @@ const style = {
   borderRadius: '50%',
   height: '80',
   alignSelf: 'flex-start',
-  marginRight: '2em'
+  marginRight: '1.6em'
 };
 
 export default ({ url }) => <img style={style} src={url} />;
