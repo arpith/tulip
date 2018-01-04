c1f077640735a0711edddb754dee2e705527b964
Arpith Siromoney <arpith@feedreader.co>
2018-01-04 23:47:23 +0530

    Support flagging messages as read

--- app/components/Messages.jsx
+++ app/components/Messages.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { throttle } from 'underscore';
 import Message from './Message';
+import { markAsRead } from '../actions';
 
-function Messages({ messages }) {
-  const message = (m) => <Message message={m} key={m.id} />;
+const listeners = {};
+const messagesToBeFlagged = [];
+
+function addListener(listener) {
+  const listenerID = Math.random();
+  listeners[listenerID] = listener;
+  return listenerID;
+}
+
+function removeListener(listenerID) {
+  delete listeners[listenerID];
+}
+
+function onScroll() {
+  Object.values(listeners).forEach(listener => listener());
+}
+
+function batchMarkAsRead(markAsRead) {
+  return (id) => {
+    messagesToBeFlagged.push(id);
+    if (messagesToBeFlagged.length > 10) {
+      const messages = messagesToBeFlagged.splice(0, 10);
+      markAsRead(messages);
+    }
+  };
+}
+
+function Messages({ messages, markAsRead }) {
+  const message = (m) => <Message message={m} 
+    key={m.id} 
+    addListener={addListener} 
+    removeListener={removeListener}
+    markAsRead={batchMarkAsRead(markAsRead)}/>;
   const style = {
     flex: 3,
     overflowX: 'scroll',
@@ -11,11 +44,23 @@ function Messages({ messages }) {
     marginRight: '0.8em',
     height: '100%'
   };
+  const throttledOnScroll = throttle(onScroll, 1000);
   return (
-    <div style={style}>
+    <div style={style} onScroll={throttledOnScroll}>
       {messages.map(message)}
     </div>
   );
 }
 
-export default connect(state => ({messages: state.messages}))(Messages);
+const mapDispatchToProps = dispatch => {
+  return {
+    markAsRead: messages => {
+      dispatch(markAsRead(messages))
+    }
+  }
+}
+
+export default connect(
+  ({messages}) => ({messages}),
+  mapDispatchToProps,
+)(Messages);
