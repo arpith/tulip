db24eddcf4da1230c06caa0307a6b4ae341e3b05
Arpith Siromoney <arpith@feedreader.co>
2018-01-05 15:09:31 +0530

    support updating pointer

--- app/components/Messages.jsx
+++ app/components/Messages.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { throttle } from 'underscore';
 import Message from './Message';
-import { markAsRead } from '../actions';
+import { markAsRead, updatePointer } from '../actions';
 
 const listeners = {};
 const messagesToBeFlagged = [];
@@ -21,22 +21,25 @@ function onScroll() {
   Object.values(listeners).forEach(listener => listener());
 }
 
-function batchMarkAsRead(markAsRead) {
+function batchMarkAsReadAndUpdatePointer(markAsRead, updatePointer) {
   return (id) => {
     messagesToBeFlagged.push(id);
     if (messagesToBeFlagged.length > 10) {
       const messages = messagesToBeFlagged.splice(0, 10);
+      const lastID = messages[messages.length - 1];
       markAsRead(messages);
+      updatePointer(lastID);
     }
   };
 }
 
-function Messages({ messages, markAsRead }) {
+function Messages({ messages, markAsRead, updatePointer }) {
   const message = (m) => <Message message={m} 
     key={m.id} 
     addListener={addListener} 
     removeListener={removeListener}
-    markAsRead={batchMarkAsRead(markAsRead)}/>;
+    markAsReadAndUpdatePointer={batchMarkAsReadAndUpdatePointer(markAsRead, updatePointer)}
+  />;
   const style = {
     flex: 3,
     overflowX: 'scroll',
@@ -56,7 +59,10 @@ const mapDispatchToProps = dispatch => {
   return {
     markAsRead: messages => {
       dispatch(markAsRead(messages))
-    }
+    },
+    updatePointer: messages => {
+      dispatch(updatePointer(messages))
+    },
   }
 }
 
