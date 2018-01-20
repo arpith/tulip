7bc1b02e0af920f50744023a8553ec4c1c06307a
Arpith Siromoney <arpith@feedreader.co>
2018-01-20 23:23:36 +0530

    current subject: display small avatar of top message

--- app/components/CurrentSubject.jsx
+++ app/components/CurrentSubject.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ThreadTitle from './ThreadTitle';
+import Avatar from './Avatar';
 
 function CurrentSubject(message) {
   return <div style={{ paddingTop: 7 }}>
+    <Avatar url={message.avatar_url} small={true} />
     <ThreadTitle {...message} />
   </div>;
 }
