4f860666e7db0d6dd4181b737dddeaf2fc6ea3e9
Arpith Siromoney <arpith@feedreader.co>
2018-01-26 12:03:24 +0100

    components/app: remove unused style, switch to functional component

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -1,19 +1,3 @@
-import React from 'react';
+import React, { cloneElement } from 'react';
 
-class App extends React.Component {
-  render() {
-    const style = {
-      float: 'left',
-      clear: 'left',
-      width: '100%'
-    };
- 
-    return (
-      <div style={style}>
-        {React.cloneElement(this.props.children)}
-      </div>
-    );
-  }
-}
-
-export default App
+export default ({ children }) => cloneElement(children);
