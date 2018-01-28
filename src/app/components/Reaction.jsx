7732ed9ee6163a39469e31f601e9420c206159b3
Arpith Siromoney <arpith@feedreader.co>
2018-01-28 15:14:30 -0500

    reactions: display counts, remove underscores from emoji name

---
+++ src/app/components/Reaction.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import EmojiConverter from 'emoji-js';
+import { wrapper, count } from '../styles/reaction';
+
+export default ({ emojiName, users }) => {
+  const emojiConverter = new EmojiConverter();
+  const reactionEmoji = emojiConverter.replace_colons(`:${emojiName.replace('_', '')}:`);
+  return (<div style={wrapper}>
+    <div>{reactionEmoji}</div>
+    <div style={count}>{users.length}</div>
+  </div>);
+}
