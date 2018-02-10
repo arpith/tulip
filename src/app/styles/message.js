8511cdb27c18738cb339ef4ff0889be270f9ed8e
Arpith Siromoney <arpith@feedreader.co>
2018-02-10 13:40:56 -0500

    styles/message: use word-wrap: break-word for inner wrapper
    This wraps long urls etc, and prevents the messages component
    from expanding (and having horizontal scroll)

--- src/app/styles/message.js
+++ src/app/styles/message.js
@@ -1,4 +1,4 @@
 import { column } from './flex';
 
 export const wrapper = { marginBottom: '1.6em', ...column };
-export const innerWrapper = { ...column, minWidth: 0 };
+export const innerWrapper = { ...column, minWidth: 0, wordWrap: 'break-word' };
