3d6914cf8b65f6574e3bfe147c40f79e4e4f2ac5
Arpith Siromoney <arpith@feedreader.co>
2018-04-19 17:33:09 -0400

    reactions: update displayed reactions after adding / removing
    constants: remove ADD_REACTION and REMOVE_REACTION
    actions: fetch message after adding / removing a reaction
    1. fetchMessages now takes optional num_before and num_after (set to 10 and
    20 otherwise).
    2. call fetchMessages if adding / removing a reaction was successful.
    
    reactions: recompute emojis / allUsers when props change
    use array for allUsers
    add emojis and allUsers to state
    use them in toggleDisplayUsers() and displayUsers()

--- src/app/components/Reactions.jsx
+++ src/app/components/Reactions.jsx
@@ -5,47 +5,58 @@ import ReactionUsers from './ReactionUsers';
 import style from '../styles/reactions';
 
 class Reactions extends Component {
-  constructor(props) {
-    super(props);
-    this.emojis = {};
-    this.users = {};
-    for (({ emoji_name, emoji_code, user }) of props.reactions) {
-      if (!this.emojis[emoji_code]) {
-        this.emojis[emoji_code] = {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      users: [],
+      displayUsers: false,
+      ...this.buildEmojisAndAllUsers(props)
+    };
+    this.toggleDisplayUsers = this.toggleDisplayUsers.bind(this);
+    this.displayUsers = this.displayUsers.bind(this);
+  }
+
+  buildEmojisAndAllUsers({ reactions, username }) {
+    const emojis = {};
+    const users = {};
+    for (({ emoji_name, emoji_code, user }) of reactions) {
+      if (!emojis[emoji_code]) {
+        emojis[emoji_code] = {
           emojiName: emoji_name,
           emojiCode: emoji_code,
           userHasReacted: false,
           users: [],
         };
       }
-      if (user.email === this.props.username) {
-        this.emojis[emoji_code].userHasReacted = true;
+      if (user.email === username) {
+        emojis[emoji_code].userHasReacted = true;
       }
-      this.users[user.email] = user;
-      this.emojis[emoji_code].users.push(user);
+      users[user.email] = user;
+      emojis[emoji_code].users.push(user);
     }
-    this.state = {
-      displayUsers: false,
-      users: []
-    };
-    this.toggleDisplayUsers = this.toggleDisplayUsers.bind(this);
-    this.displayUsers = this.displayUsers.bind(this);
+    return { emojis, allUsers: Object.values(users) };
+  }
+
+  componentWillReceiveProps(props) {
+    this.setState(this.buildEmojisAndAllUsers(props));
   }
 
   toggleDisplayUsers() {
-    if (!this.state.displayUsers) {
-      this.setState({ users: Object.values(this.users) });
-    }
-    this.setState({ displayUsers: !this.state.displayUsers });
+    this.setState(({ allUsers, displayUsers }) => {
+      if (!displayUsers) {
+        return { users: allUsers, displayUsers: true };
+      }
+      return { displayUsers: false };
+    });
   }
 
   displayUsers(emojiCode) {
-    this.setState({ users: this.emojis[emojiCode].users });
+    this.setState(({ emojis }) => ({users: emojis[emojiCode].users}));
   }
 
   render() {
     return <div style={style} onMouseEnter={this.toggleDisplayUsers} onMouseLeave={this.toggleDisplayUsers} >
-      <ReactionsList emojis={Object.values(this.emojis)}
+      <ReactionsList emojis={Object.values(this.state.emojis)}
         onHover={this.displayUsers}
         messageID={this.props.messageID}
       />
@@ -53,6 +64,7 @@ class Reactions extends Component {
     </div>;
   }
 }
+
 export default connect(
   ({config}) => ({username: config.username}),
 )(Reactions);
