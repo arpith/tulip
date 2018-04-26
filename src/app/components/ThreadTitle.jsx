5fff778e9d63a368d8b1d3e51e9aad64f5cdf33b
Arpith Siromoney <arpith@feedreader.co>
2018-04-26 13:30:22 -0400

    ThreadTitle: support emojis

--- src/app/components/ThreadTitle.jsx
+++ src/app/components/ThreadTitle.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { replaceColons } from '../emoji';
 import style from '../styles/threadTitle';
 import Recipients from './Recipients';
 import ThreadSubject from './ThreadSubject';
@@ -6,6 +7,6 @@ import ThreadSubject from './ThreadSubject';
 export default ({ display_recipient, subject }) => {
   return <div style={style}>
     <Recipients recipients={display_recipient} />
-    <ThreadSubject subject={subject} />
+    <ThreadSubject subject={replaceColons(subject)} />
   </div>;
 }
